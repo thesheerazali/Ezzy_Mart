0c894229d9cc20a50c2a0b168140462bada78760
thesheerazali <sheerazali7676@gmail.com>
2024-01-20 19:10:57 +0500

    Product data

--- lib/services/firestore_services.dart
+++ lib/services/firestore_services.dart
@@ -1,12 +1,17 @@
 import 'package:my_mart/consts/firebase_const.dart';
 
 class FireStoreServices {
-
   //get users data
   static getUserData(uid) {
     return firestore
         .collection(usersCollection)
         .where('id', isEqualTo: uid)
         .snapshots();
+  }
+
+  static getProducts(category) {
+    return firestore
+        .collection(productsCollection)
+        .where('p_category', isEqualTo: category).snapshots();
   }
 }
