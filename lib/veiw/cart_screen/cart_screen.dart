d49dd77b0ab857ebad41556505fb7c217adf93ba
thesheerazali <sheerazali7676@gmail.com>
2024-01-21 18:05:20 +0500

    fetched cart detail from firestore and showed in cart screen

--- lib/veiw/cart_screen/cart_screen.dart
+++ lib/veiw/cart_screen/cart_screen.dart
@@ -1,20 +1,116 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:my_mart/Common_Widgets/loading_indicator.dart';
+import 'package:my_mart/Common_Widgets/out_button.dart';
 import 'package:my_mart/consts/consts.dart';
-
+import 'package:my_mart/consts/firebase_const.dart';
+import 'package:my_mart/controllers/cart_controller.dart';
+import 'package:my_mart/services/firestore_services.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      color: Colors.white,
-      child: "Cart Is Empty"
-          .text
-          .fontFamily(semibold)
-          .color(darkFontGrey)
-          .makeCentered(),
-    );
+    var controller = Get.put(CartController());
+    return Scaffold(
+        backgroundColor: whiteColor,
+        appBar: AppBar(
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          title: "Shopping Cart"
+              .text
+              .color(darkFontGrey)
+              .fontFamily(semibold)
+              .make(),
+        ),
+        body: StreamBuilder(
+          stream: FireStoreServices.getCart(currentUser!.uid),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return Center(child: loadingIndicator());
+            } else if (snapshot.data!.docs.isEmpty) {
+              return Center(
+                  child: "Cart is Empty"
+                      .text
+                      .color(darkFontGrey)
+                      .fontFamily(semibold)
+                      .make());
+            } else {
+              var data = snapshot.data!.docs;
+              controller.calculateTPrice(data);
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Expanded(
+                        child: ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: Image.network("${data[index]['img']}"),
+                          title:
+                              "${data[index]['title']} x${data[index]['qty']}"
+                                  .text
+                                  .fontFamily(semibold)
+                                  .size(16)
+                                  .make(),
+                          subtitle: "${data[index]['tprice']}"
+                              .numCurrency
+                              .text
+                              .color(redColor)
+                              .fontFamily(semibold)
+                              .make(),
+                          trailing: const Icon(
+                            Icons.delete,
+                            color: redColor,
+                          ).onTap(() {
+                            print("delete");
+                            FireStoreServices.deleteDoc(data[index].id);
+                          }),
+                        );
+                      },
+                    )),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        "Total Price"
+                            .text
+                            .fontFamily(semibold)
+                            .color(darkFontGrey)
+                            .make(),
+                        Obx(
+                          () => "${controller.totalP.value}"
+                              .numCurrency
+                              .text
+                              .fontFamily(semibold)
+                              .color(redColor)
+                              .make(),
+                        )
+                      ],
+                    )
+                        .box
+                        .padding(const EdgeInsets.all(12))
+                        .color(lightGolden)
+                        .width(context.screenWidth - 60)
+                        .roundedSM
+                        .make(),
+                    10.heightBox,
+                    SizedBox(
+                      width: (context.screenWidth - 60),
+                      child: ourButton(
+                          color: redColor,
+                          onPress: () {},
+                          radiusValue: 10.0,
+                          textColor: whiteColor,
+                          title: "Proceed To Shipping"),
+                    )
+                  ],
+                ),
+              );
+            }
+          },
+        ));
   }
 }
